c96c883b9500210231fb883280f27ba9873a6f9c
trackwocc <168234184+trackwocc@users.noreply.github.com>
2025-05-24 19:13:20 -0400

    Update loadstring.lua

--- outfit-changer-gui/loadstring.lua
+++ outfit-changer-gui/loadstring.lua
@@ -1,7 +1,7 @@
 getgenv().Configuration = {
     showKeybind = 'T', -- keybind para esconder/mostrar el gui
-    breakJointsOnDead = false, -- si deberia desarmar al personaje cuando muere
-    changeAnimations = false -- si debe cambiar las animaciones a las del fake player
+    breakJointsOnDead = true, -- si deberia desarmar al personaje cuando muere
+    changeAnimations = true -- si debe cambiar las animaciones a las del fake player
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/mogolicoo/techware/main/outfit-changer-gui/source.lua"))()
